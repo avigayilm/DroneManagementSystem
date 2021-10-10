507b50758e829303f7400effc3111ca45a39dc8a
avigayilm <amandel@g.jct.ac.il>
2021-10-10 21:21:42 +0300

    Initial code +Welcome functions

---
+++ Targil0/Program9033.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome9033();
+            Welcome6996();
+            Console.ReadKey();
+        }
+
+
+        private static void Welcome9033()
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+
+        static partial void Welcome6996();
+    }
+}
