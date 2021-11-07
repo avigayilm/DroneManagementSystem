6dbb4c2687fedc6766d9eee90771005d3edc0473
hudis flax <yf56229@gmail.com>
2021-11-07 20:04:34 +0200

    added customer function to BL

---
+++ BL/CustomerToList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBL
+{
+    namespace BO
+    {
+        class CustomerToList
+        {
+            public string id;
+            public string name;
+            public string phoneNumber;
+            public int NumPacksReceived;
+            public int NumPackSentDel;
+            public int NumPackSentUnDel;
+            public int NumPackExp;
+
+        }
+    }
+}
