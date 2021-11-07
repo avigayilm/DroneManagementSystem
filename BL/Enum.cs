d704121c56dcced46451f3613d055476a9deeb01
avigayilm <amandel@g.jct.ac.il>
2021-11-07 20:14:23 +0200

    updated all entities functions

--- BL/Enum.cs
+++ BL/Enum.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace BL
+namespace IBL
 {
-    public class Enum
+    namespace BO
     {
-        public enum DroneStatuses
-        {
-            Available, Maintenance, Delivery
-        }
+            public enum DroneStatuses
+            {
+                Available, Maintenance, Delivery
+            }
+            public enum WeightCategories
+            {
+                light = 0, medium, heavy
+            }
+
+            public enum Priorities
+            {
+                normal = 0, fast, emergency
+            }
     }
 }
--- BL/Parcel.cs
+++ BL/Parcel.cs
@@ -3,13 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using IDAL.DO;
 
 namespace IBL
 {
     namespace BO
     {
-        public class package
+        public class Parcel
         {
             public int id;
             public Customer sender;
