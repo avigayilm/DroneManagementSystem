b3c712f364d282b84133ee3614f8cf2e13c427dd
avigayilm <amandel@g.jct.ac.il>
2021-11-30 13:35:02 +0200

    worked on the update functions

--- PL/DroneWindow.xaml.cs
+++ PL/DroneWindow.xaml.cs
@@ -19,10 +19,15 @@ namespace PL
     /// <summary>
     /// Interaction logic for DroneWindow.xaml
     /// </summary>
+
+    public enum UpdateOptions
+    {
+        updateModel,SendingToCharge, ReleaseFromCharge,Delivery,CollectingAParcel,DeliveringAParcel
+    }
     public partial class DroneWindow : Window
     {
         IBL.Ibl bl;
-        private int id;
+        internal int id;
         private int StationId;
         private IBL.BO.WeightCategories weight;
         private IBL.BO.Drone droneTemp ;
@@ -43,6 +48,8 @@ namespace PL
             ltTx.IsEnabled = false;
             mTb.IsEnabled = false;
             submit.Content = "Add Drone";
+            ComboUpdateOption.Visibility = Visibility.Collapsed;// doesn't show the update option
+            choice = "add";
             //DronesListView.ItemsSource = bl.GetAllDrones();
             //StatusSelector.ItemsSource = Enum.GetValues(typeof(DroneStatuses));
             //WeightSelector.ItemsSource = Enum.GetValues(typeof(WeightCategories));
@@ -53,7 +60,16 @@ namespace PL
         {
             InitializeComponent();
             bl = ibl;
+            idTx.Text = (dr.Id).ToString();
+            wCb.SelectedIndex = (int)dr.Weight;
+            statCb.SelectedIndex = (int)dr.Status;
+            dTx.Text = (dr.ParcelId).ToString();
+            lnTx.Text = (dr.Loc.Longitude).ToString();
+            ltTx.Text = (dr.Loc.Latitude).ToString();
+            mTx.Text = dr.Model;
             submit.Content = "Update Drone";
+            choice = "update";
+            ComboUpdateOption.ItemsSource= Enum.GetValues(typeof(UpdateOptions));
         }
 
         private void AddDrone()
@@ -73,10 +89,71 @@ namespace PL
 
         private void submit_Click(object sender, RoutedEventArgs e)
         {
-           AddDrone();
-           MessageBox.Show(droneTemp.ToString());
-        }
+            try
+            {
+                UpdateOptions inputedOption = (UpdateOptions)ComboUpdateOption.SelectedItem;
+                if (choice == "add")
+                    AddDrone();
+                if (choice == "update")
+                {
+                    switch (inputedOption)
+                    {
+                        case UpdateOptions.SendingToCharge:
+                            {
 
+                                bl.SendingDroneToCharge(int.Parse(idTx.Text));
+                                break;
+                            }
+                        case UpdateOptions.ReleaseFromCharge:
+                            {
+                                bl.ReleasingDroneFromCharge(int.Parse(idTx.Text), double.Parse(addMinutestxt.Text));
+                                break;
+                            }
+                        case UpdateOptions.Delivery:
+                            {
+                                bl.AssignParcelToDrone(int.Parse(idTx.Text));
+                                break;
+                            }
+                        case UpdateOptions.CollectingAParcel:
+                            {
+                                bl.CollectingParcelByDrone(int.Parse(idTx.Text));
+                                break;
+                            }
+                        case UpdateOptions.DeliveringAParcel:
+                            {
+                                bl.DeliverParcelByDrone(int.Parse(idTx.Text));
+                                break;
+                            }
+                        case UpdateOptions.updateModel:
+                            {
+                                bl.UpdateDrone(int.Parse(idTx.Text), mTx.Text);
+                                break;
+                            }
+                    }
+                }
+                MessageBox.Show(droneTemp.ToString());
+            }
+            catch(AddingException ex)
+            {
+                _ = MessageBox.Show(ex.Message, "Error", MessageBoxButton.OKCancel, MessageBoxImage.Error);
+        //switch (messageBoxResult)       
+        //        {
+        //            case MessageBoxResult.None:
+        //                break;
+        //            case MessageBoxResult.OK:
+        //                Close();             
+        //                break;
+        //            case MessageBoxResult.Cancel:
+        //                break;
+        //            case MessageBoxResult.Yes:
+        //                break;
+        //            case MessageBoxResult.No:
+        //                break;
+        //            default:
+        //                break;
+        //        }
+            }
+        }
         private void mTx_TextChanged(object sender, TextChangedEventArgs e)
         {
             submit.IsEnabled = true;
@@ -100,6 +177,44 @@ namespace PL
         private void sTx_TextChanged(object sender, TextChangedEventArgs e)
         {
             EnableSubmit();
+        }
+
+        private void UpdateOption_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            EnableAllKeysbutId();
+            UpdateOptions inputedOption = (UpdateOptions)ComboUpdateOption.SelectedItem;
+            if(inputedOption==UpdateOptions.ReleaseFromCharge)
+            {
+                addMinutestxt.Visibility = Visibility.Visible;
+                addMinutes.Visibility = Visibility.Visible;
+            }
+            if(inputedOption==UpdateOptions.updateModel)
+            {
+                mTx.IsEnabled = true;
+
+            }
+            if(idTx.Text != string.Empty)
+            {
+                submit.IsEnabled = true;
+            }
+        }
+
+        private void EnableAllKeysbutId()
+        {
+            mTb.IsEnabled = false;
+            ltTb.IsEnabled = false;
+            lnTb.IsEnabled = false;
+            dTb.IsEnabled = false;
+            statTb.IsEnabled = false;
+            mTx.IsEnabled = false;
+            statCb.IsEnabled = false;
+            dTx.IsEnabled = false;
+            lnTx.IsEnabled = false;
+            ltTx.IsEnabled = false;
+            sTb.IsEnabled = false;
+            sTx.IsEnabled = false;
+            wTb.IsEnabled = false;
+            wCb.IsEnabled = false;
         }
     }
 }
