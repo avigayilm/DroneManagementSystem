b07a746a3d65951713b6f0465b98deb01c8af8a8
avigayilm <amandel@g.jct.ac.il>
2022-01-02 21:54:25 +0200

    worked on DroneXml

--- DalXml/DalXml/DalXmlDrone.cs
+++ DalXml/DalXml/DalXmlDrone.cs
@@ -10,24 +10,132 @@ namespace Dal
 {
     internal sealed partial class DalXml
     {
+
+        /// <summary>
+        /// adds a drone to the dronlist
+        /// </summary>
+        /// <param name="dro"></param>
         public void AddDrone(Drone dro)
         {
-            throw new NotImplementedException();
+            List<Drone> drones = XMLTools.LoadListFromXMLSerializer<Drone>(DroneXml);
+            if (drones.Exists(d => d.Id == d.Id))
+                throw new DuplicateIdException("Drone already exists\n");
+            drones.Add(dro);
+            XMLTools.SaveListToXMLSerializer(drones, DroneXml);
         }
 
-        public Drone GetDrone(int ID)
+        ///// <summary>
+        ///// function that changes the status of the drone according to the given parameter.
+        ///// </summary>
+        ///// <param name="DroneId"></param>
+        ///// <param name="st"></param>
+        //public void ChangeDroneStatus(int DroneId, DroneStatuses st)
+        //{
+        //    int droneIndex = DataSource.dronesList.FindIndex(d => d.id == DroneId);
+        //    if (droneIndex == -1)
+        //        throw new MissingIdException("No such drone\n");
+        //    else
+        //    {
+        //        var temp = DataSource.dronesList[droneIndex];
+        //        temp.status = st;
+        //        DataSource.dronesList[droneIndex] = temp;
+        //    }
+        //}
+
+        /// <summary>
+        /// returns the drone according to the given Id
+        /// </summary>
+        /// <param name="ID"></param>
+        /// <returns></returns>
+        public Drone GetDrone(int droneId)
         {
-            throw new NotImplementedException();
+           // XMLTools.LoadListFromXMLSerializer<Drone>(DroneXml)
+            int index = CheckExistingDrone(droneId);
+            return DataSource.dronesList[index];
         }
+
+
         public IEnumerable<Drone> GetAllDrones(Predicate<Drone> predicate = null)
         {
-            throw new NotImplementedException();
+            return XMLTools.LoadListFromXMLSerializer<Drone>(DroneXml).FindAll(d => predicate == null ? true : predicate(d));
         }
-
+        /// <summary>
+        /// updates the model of the drone, used in BL
+        /// </summary>
+        /// <param name="droneId"></param>
+        /// <param name="model"></param>
         public void UpdateDrone(int droneId, string model)
         {
-            throw new NotImplementedException();
+            List<Drone> drones = XMLTools.LoadListFromXMLSerializer<Drone>(DroneXml);
+            int index = drones.FindIndex(d => d.Id == droneId);
+            if(index==-1)
+            {
+                throw new MissingIdException("No such Drone exists\n");
+            }
+            Drone tempDrone = drones[index];
+            tempDrone.Model = model;
+            drones[index] = tempDrone;
+            XMLTools.SaveListToXMLSerializer(drones, DroneXml);
         }
+        /// <summary>
+        /// returns a list of all the drones charging at a station
+        /// </summary>
+        /// <param name="stationId"></param>
+        /// <returns></returns>
+        public IEnumerable<Drone> DronesChargingAtStation(int stationId)
+        {
+            List<Drone> charging = new List<Drone>();
+            charging= XMLTools.LoadListFromXMLSerializer<DroneCharge>(DroneChargeXml).ForEach(d => { if (d.StationId == stationId) charging.Add(GetDrone(d.DroneId)); });
+
+
+        }
+
+        /// <summary>
+        /// checks if a customer exists in the customerlist, if it doesn't it throws a MissingIdException
+        /// </summary>
+        /// <param name="customerId"></param>
+        /// <returns></returns>
+        internal int CheckExistingDrone(int droneId)
+        {
+            List<Drone> drones = XMLTools.LoadListFromXMLSerializer<Drone>(DroneXml);
+            int index = drones.FindIndex(d => d.Id == droneId);
+            if (index == -1)
+            {
+                throw new MissingIdException("No such Drone exists\n");
+            }
+            return index;
+        }
+
+        /// <summary>
+        /// checks if a customer already exists, if it does it throws a duplicateIdException
+        /// </summary>
+        /// <param name="customerId"></param>
+        public void CheckDuplicateDrone(int droneId)
+        {
+            List<Drone> drones = XMLTools.LoadListFromXMLSerializer<Drone>(DroneXml);
+            if (drones.Exists(d => d.Id == droneId))
+            {
+                throw new DuplicateIdException("Drone already exists\n");
+            }
+        }
+        //public void AddDrone(Drone dro)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public Drone GetDrone(int ID)
+        //{
+        //    throw new NotImplementedException();
+        //}
+        //public IEnumerable<Drone> GetAllDrones(Predicate<Drone> predicate = null)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public void UpdateDrone(int droneId, string model)
+        //{
+        //    throw new NotImplementedException();
+        //}
 
 
     }
--- DalXml/DalXml/DalXmlDroneCharge.cs
+++ DalXml/DalXml/DalXmlDroneCharge.cs
@@ -11,24 +11,24 @@ namespace Dal
     internal sealed partial class DalXml
     {
 
-        public void SendToCharge(int droneId, int stationId)
-        {
-            throw new NotImplementedException();
-        }
+        //public void SendToCharge(int droneId, int stationId)
+        //{
+        //    throw new NotImplementedException();
+        //}
 
-        public void BatteryCharged(int droneId, int stationId)
-        {
-            throw new NotImplementedException();
-        }
+        //public void BatteryCharged(int droneId, int stationId)
+        //{
+        //    throw new NotImplementedException();
+        //}
 
 
-        public IEnumerable<DroneCharge> GetDroneChargeList(Predicate<DroneCharge> predicate = null)
-        {
-            throw new NotImplementedException();
-        }
-        public IEnumerable<Drone> DronesChargingAtStation(int stationId)
-        {
-            throw new NotImplementedException();
-        }
+        //public IEnumerable<DroneCharge> GetDroneChargeList(Predicate<DroneCharge> predicate = null)
+        //{
+        //    throw new NotImplementedException();
+        //}
+        //public IEnumerable<Drone> DronesChargingAtStation(int stationId)
+        //{
+        //    throw new NotImplementedException();
+        //}
     }
 }
